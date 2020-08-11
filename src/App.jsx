f388e22f2e29f44a8ab4bce02e238d21dd4370a2
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-11 11:21:01 +0200

    scaffolding app, cleaning up and adding functional component usuing props

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = (props) => {
+  return (
+    <>
+      <h1>{props.message}</h1>
+    </>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App message="Hello world from props"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
